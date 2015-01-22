419a778562b7c92cfb7d0dc6bf074af8277cb9b0
mmmyum <matt@mattsaunders.info>
2015-01-21 21:18:41 -0500

    Added more to pokemon objects - stats, evolutions

--- app/src/main/java/info/mattsaunders/apps/dexterous/LoadPokemon.java
+++ app/src/main/java/info/mattsaunders/apps/dexterous/LoadPokemon.java
@@ -1,5 +1,6 @@
 package info.mattsaunders.apps.dexterous;
 
+import android.os.Bundle;
 import android.util.Log;
 
 import org.json.JSONArray;
@@ -20,8 +21,19 @@ public class LoadPokemon {
             String name = "";
             String type1 = "";
             String type2 = "";
+            int hp = 0;
+            int atk = 0;
+            int def = 0;
+            int spatk = 0;
+            int spdef = 0;
+            int spd = 0;
+            String height = "";
+            String weight = "";
+            //Bundle[] evolutions = new Bundle[0];
+            ArrayList<Bundle> evolutions = new ArrayList();
             //Try to parse JSON object:
             try {
+                //Get name:
                 name = jsonObject.getString("name");
                 //Get JSONArray of types:
                 JSONArray types = jsonObject.getJSONArray("types");
@@ -29,10 +41,83 @@ public class LoadPokemon {
                 if (types.length() > 1) {
                     type2 = types.getJSONObject(1).getString("name");
                 }
+                //Get stats:
+                hp = jsonObject.getInt("hp");
+                atk = jsonObject.getInt("attack");
+                def = jsonObject.getInt("defense");
+                spatk = jsonObject.getInt("sp_atk");
+                spdef = jsonObject.getInt("sp_def");
+                spd = jsonObject.getInt("speed");
+                //Get height and weight:
+                height = jsonObject.getString("height");
+                weight = jsonObject.getString("weight");
+                //Get JSONArray of evolutions:
+                JSONArray evos = jsonObject.getJSONArray("evolutions");
+                if (evos.length() > 0) {
+                    int level = 0;
+                    String method;
+                    String to;
+                    Bundle tempBundle = new Bundle();
+
+                    for (int j = 0; j < evos.length(); j++) {
+                        JSONObject tempobj = evos.getJSONObject(j);
+                        level = tempobj.getInt("level");
+                        method = tempobj.getString("method");
+                        to = tempobj.getString("to");
+                        /*
+                        System.out.println("Name        -------------------------" + name);
+                        System.out.println("Bundle Array-------------------------" + evolutions.size());
+                        System.out.println("JSON Array  -------------------------" + evos.length());
+                        System.out.println("Level       -------------------------" + level);
+                        System.out.println("Method      -------------------------" + method);
+                        System.out.println("To          -------------------------" + to);
+                        */
+                        if (tempobj.has("level")) {
+                            //evolutions.get(j).putInt("level", level);
+                            tempBundle.putInt("level", level);
+                            System.out.println("Put level");
+                        }
+                        if (method != null) {
+                            //evolutions.get(j).putString("method", method);
+                            tempBundle.putString("method", method);
+                            System.out.println("Put method");
+                        }
+                        if (to != null) {
+                            //evolutions.get(j).putString("to", to);
+                            tempBundle.putString("to", to);
+                            System.out.println("Put to");
+                        }
+                        evolutions.add(tempBundle);
+                        /*
+                        if (evos.getJSONObject(j).has("level")) {
+                            evolutions[j].putInt("level", evos.getJSONObject(j).getInt("level"));
+                        }
+                        if (evos.getJSONObject(j).has("method")) {
+                            evolutions[j].putString("method", evos.getJSONObject(j).getString("method"));
+                        }
+                        if (evos.getJSONObject(j).has("to")) {
+                            evolutions[j].putString("to", evos.getJSONObject(j).getString("to"));
+                        }
+                        */
+                    }
+
+                }
             } catch (JSONException e) {
                 Log.e("JSON LOADING", "Failed to read JSON: " + e);
             }
-            pokelist.add(new Pokemon(i,name,type1,type2));
+            pokelist.add(new Pokemon(i,
+                    name,
+                    type1,
+                    type2,
+                    hp,
+                    atk,
+                    def,
+                    spatk,
+                    spdef,
+                    spd,
+                    height,
+                    weight,
+                    evolutions));
         }
         for (Pokemon poke : pokelist) {
             System.out.println(poke.getSummary());
--- app/src/main/java/info/mattsaunders/apps/dexterous/Pokemon.java
+++ app/src/main/java/info/mattsaunders/apps/dexterous/Pokemon.java
@@ -1,5 +1,9 @@
 package info.mattsaunders.apps.dexterous;
 
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
 /**
  * Pokemon object to store info for pokemon
  */
@@ -8,12 +12,42 @@ public class Pokemon {
     private String name;
     private String typeOne;
     private String typeTwo;
+    private int hp;
+    private int atk;
+    private int def;
+    private int spatk;
+    private int spdef;
+    private int spd;
+    private String height;
+    private String weight;
+    private ArrayList<Bundle> evolutions;
 
-    public Pokemon(int number, String name, String typeOne, String typeTwo) {
+    public Pokemon(int number,
+                   String name,
+                   String typeOne,
+                   String typeTwo,
+                   int hp,
+                   int atk,
+                   int def,
+                   int spatk,
+                   int spdef,
+                   int spd,
+                   String height,
+                   String weight,
+                   ArrayList<Bundle> evolutions) {
         this.number = number;
         this.name = name;
         this.typeOne = typeOne;
         this.typeTwo = typeTwo;
+        this.hp = hp;
+        this.atk = atk;
+        this.def = def;
+        this.spatk = spatk;
+        this.spdef = spdef;
+        this.spd = spd;
+        this.height = height;
+        this.weight = weight;
+        this.evolutions = evolutions;
     }
 
     public int getNumber() { return number; }
@@ -21,5 +55,14 @@ public class Pokemon {
     public String getName() { return name; }
     public String getTypeOne() { return typeOne; }
     public String getTypeTwo() { return typeTwo; }
-    public String getSummary() { return String.valueOf(number) + " " + name + " " + typeOne + " " + typeTwo; }
+    public String getSummary() {
+        String evolves = "";
+        for (Bundle evo : evolutions) {
+            if (evo.containsKey("to")) {
+                evolves = evolves + evo.getString("to");
+            }
+        }
+        return String.valueOf(number) + " " + name + " " + typeOne + " " + typeTwo + " -- " + evolves + "\n"
+                + "__________" + " HP: " + hp + " ATK: " + atk + " DEF: " + def + " SPATK: " + spatk + " SPDEF: " + spdef + " SPD " + spd;
+    }
 }
