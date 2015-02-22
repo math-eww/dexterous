47b1920cf678fae549e772588cc7d2555ac9a367
mmmyum <matt@mattsaunders.info>
2015-02-21 20:05:58 -0500

    Cleaned up move details dialog

--- app/src/main/java/info/mattsaunders/apps/dexterous/MoveDialogFragment.java
+++ app/src/main/java/info/mattsaunders/apps/dexterous/MoveDialogFragment.java
@@ -2,14 +2,16 @@ package info.mattsaunders.apps.dexterous;
 
 import android.app.DialogFragment;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 /**
- * Created by Matt on 21/02/2015.
+ * Created by Matt on 21/02/2015. Shows details of a move
  */
 public class MoveDialogFragment extends DialogFragment {
     @Override
@@ -22,6 +24,13 @@ public class MoveDialogFragment extends DialogFragment {
         {
             textView.setGravity(Gravity.CENTER);
         }
+/*
+        DisplayMetrics metrics = MainActivity.c.getResources().getDisplayMetrics();
+        int width = metrics.widthPixels;
+        int height = (int) Math.round(metrics.heightPixels * 0.8);
+
+        getDialog().getWindow().setLayout(width,height);
+*/
     }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -39,15 +48,33 @@ public class MoveDialogFragment extends DialogFragment {
         TextView effects = (TextView) rootView.findViewById(R.id.dialog_effect);
         TextView effectChance = (TextView) rootView.findViewById(R.id.dialog_effectchance);
 
+        LinearLayout topContainer = (LinearLayout) rootView.findViewById(R.id.dialog_container);
+
+
         damageType.setText(details.getString("damageType"));
         type.setText(details.getString("type"));
-        power.setText(String.valueOf(details.getInt("power")));
-        accuracy.setText(String.valueOf(details.getInt("accuracy")));
+
+        int powerInt = details.getInt("power");
+        if (powerInt != 0) { power.setText(String.valueOf(powerInt)); }
+        else { topContainer.removeView(rootView.findViewById(R.id.dialog_powerContainer)); }
+
+        int accuracyInt = details.getInt("accuracy");
+        if (accuracyInt != 0) { accuracy.setText(String.valueOf(accuracyInt)); }
+        else { topContainer.removeView(rootView.findViewById(R.id.dialog_accuracyContainer)); }
+
         pp.setText(String.valueOf(details.getInt("pp")));
-        priority.setText(String.valueOf(details.getInt("priority")));
+
+        int priorityInt = details.getInt("priority");
+        if (priorityInt != 0) { priority.setText(String.valueOf(priorityInt)); }
+        else { topContainer.removeView(rootView.findViewById(R.id.dialog_priorityContainer)); }
+
         targets.setText(details.getString("targets"));
         effects.setText(details.getString("effect"));
-        effectChance.setText(String.valueOf(details.getInt("effectChance")));
+
+        int effectChanceInt = details.getInt("effectChance");
+        if (effectChanceInt != 0) { effectChance.setText(String.valueOf(effectChanceInt) + "%"); }
+        else { topContainer.removeView(rootView.findViewById(R.id.dialog_effectChanceContainer)); }
+
 
         String name = details.getString("name");
         name = name.substring(0, 1).toUpperCase() + name.substring(1);
