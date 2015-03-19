5a11d3d2c09076bae63265f28595708f18bad0fd
mmmyum <matt@mattsaunders.info>
2015-03-18 21:54:37 -0400

    PreferencesActivity - confirmation dialog added

--- app/src/main/java/info/mattsaunders/apps/dexterous/PreferencesActivity.java
+++ app/src/main/java/info/mattsaunders/apps/dexterous/PreferencesActivity.java
@@ -1,5 +1,7 @@
 package info.mattsaunders.apps.dexterous;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -21,21 +23,44 @@ public class PreferencesActivity extends ActionBarActivity {
         redownloadSprites.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                finish();
-                new DownloadSprites.CallAPI().execute();
+                confirmationDialog("redownload Pokemon sprites",0);
             }
         });
         backupMarkers.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Utilities.backupPokeballIndicators();
+                confirmationDialog("backup Pokedex markers",1);
             }
         });
         restoreMarkers.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Utilities.restorePokeballIndicators();
+                confirmationDialog("restore Pokedex markers",2);
             }
         });
+    }
+
+    private void confirmationDialog(String confirmWhat, final int actionToDo) {
+        new AlertDialog.Builder(this)
+                .setMessage("Are you sure you want to " + confirmWhat + "?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        switch (actionToDo) {
+                            case 0:
+                                finish();
+                                new DownloadSprites.CallAPI().execute();
+                                break;
+                            case 1:
+                                Utilities.backupPokeballIndicators();
+                                break;
+                            case 2:
+                                Utilities.restorePokeballIndicators();
+                                break;
+                        }
+                    }
+                })
+                .setNegativeButton("No", null)
+                .show();
     }
 }
